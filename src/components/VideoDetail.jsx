c6fb3ec30007bc9d913dc888fd12ad148e760471
christian-naykene <11859693+christian-naykene@users.noreply.github.com>
2025-02-05 22:53:42 +0000

    build videoDetails

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Box, Stack } from "@mui/material";
+import React, { useEffect } from "react";
+import ReactPlayer from "react-player";
+import { useParams } from "react-router-dom";
 
 const VideoDetail = () => {
-  return (
-    <div>VideoDetail</div>
-  )
-}
+  const [videoDetail, setVideoDetail] = React.useState([]);
+  const { id } = useParams();
 
-export default VideoDetail
+  useEffect(() => {
+    fetch(`videos?part=snippet,statistics&id=${id}`).then((data) => {
+      if (data.items) {
+        setVideoDetail(data.items[0]);
+      }
+    });
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default VideoDetail;
